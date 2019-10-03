869551ac687bd11231401d486a5c41c9dbce93b2
Andrei Tcibin <tsibin.andr@gmail.com>
2019-10-03 23:04:20 +0300

    Change entrypoint workdir

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,8 @@
 
 root_file="$1"
 
-ls -la /
-ls -la /root
-ls -la /github/home
-ls -la /github/workflow
-ls -la /github/workspace
+cd /github/workspace
+mkdir outputs
 
 pdflatex -output-directory="outputs" \
          "$root_file"
