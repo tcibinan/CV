8b7ef432bd2bd1bd665f827a0b580cfc4680810f
Andrei Tcibin <tsibin.andr@gmail.com>
2019-10-03 22:57:02 +0300

    Add she-bang line and additional listing dirs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,12 @@
+#!/bin/bash
+
 root_file="$1"
 
 ls -la /
 ls -la /root
+ls -la /github/home
+ls -la /github/workflow
+ls -la /github/workspace
 
 pdflatex -output-directory="outputs" \
          "$root_file"
